7acc53f309f6546d57a641ff0bc925c7aa373ee0
Thimble <anthonyeboothroyd@gmail.com>
2020-10-10 19:00:53 +0100

    Changed Tileset

--- Roguelike-master/Roguelike/Assets/Scripts/BoardManager.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/BoardManager.cs
@@ -8,15 +8,18 @@ public class BoardManager : MonoBehaviour
     public int width = 64;
     public int height = 64;
 
-    public Tilemap tileMapGround;
-    public Tilemap tileMapWall;
-    public TileBase floor;
-    public TileBase wall;
+    public Tilemap tileMap;
+
+    public TileBase[] floor;
+    public TileBase[] north_walls;
+    public TileBase[] east_walls;
+    public TileBase[] south_walls;
+    public TileBase[] west_walls;
+    public TileBase[] corners;
+
 
     private void Awake()
     {
-        //tileMapGround.BoxFill( Vector3Int.zero, wall, -25, -25, 25, 25 );
-
         MapFactory.Type[,] rooms = MapFactory.BuildFloor( width, height, roomCount );
 
         for ( int y = 0; y < height; y++ )
@@ -26,10 +29,71 @@ public class BoardManager : MonoBehaviour
                 switch ( rooms[x, y] )
                 {
                     case MapFactory.Type.wall:
-                        tileMapGround.SetTile( new Vector3Int( x, y, 0 ), wall );
+
+                        if ( rooms[x + 1, y] == MapFactory.Type.wall && rooms[x, y - 1] == MapFactory.Type.wall )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), west_walls[Random.Range( 0, west_walls.Length )] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.wall && rooms[x, y - 1] == MapFactory.Type.wall )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), east_walls[Random.Range( 0, east_walls.Length )] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.wall && rooms[x, y - 1] == MapFactory.Type.wall &&
+                             rooms[x + 1, y - 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), west_walls[Random.Range( 0, west_walls.Length )] );
+
+                        if ( rooms[x + 1, y] == MapFactory.Type.wall && rooms[x, y - 1] == MapFactory.Type.wall &&
+                             rooms[x - 1, y - 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), east_walls[Random.Range( 0, east_walls.Length )] );
+
+                        if ( rooms[x, y - 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), north_walls[Random.Range( 0, north_walls.Length )] );
+
+                        if ( rooms[x, y + 1] == MapFactory.Type.floor && rooms[x, y-1] != MapFactory.Type.floor)
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), south_walls[Random.Range( 0, south_walls.Length )] );
+
+                        if ( rooms[x + 1, y] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), west_walls[Random.Range( 0, west_walls.Length )] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), east_walls[Random.Range( 0, east_walls.Length )] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.floor && rooms[x, y - 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), north_walls[Random.Range( 0, north_walls.Length )] );
+
+                        if ( rooms[x + 1, y] == MapFactory.Type.floor && rooms[x, y - 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), north_walls[Random.Range( 0, north_walls.Length )] );
+
+                        if ( rooms[x + 1, y] == MapFactory.Type.wall && rooms[x, y + 1] == MapFactory.Type.wall &&
+                             rooms[x + 1, y + 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), corners[0] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.wall && rooms[x, y + 1] == MapFactory.Type.wall &&
+                             rooms[x - 1, y + 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), corners[1] );
+
+                        if ( rooms[x + 1, y] == MapFactory.Type.wall && rooms[x, y - 1] == MapFactory.Type.wall &&
+                             rooms[x, y + 1] == MapFactory.Type.floor && rooms[x - 1, y] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), corners[2] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.wall && rooms[x, y - 1] == MapFactory.Type.wall &&
+                             rooms[x, y + 1] == MapFactory.Type.floor && rooms[x + 1, y] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), corners[3] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.floor && rooms[x + 1, y] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), corners[6] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.floor && rooms[x + 1, y] == MapFactory.Type.floor &&
+                             rooms[x, y + 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), corners[7] );
+
+                        if ( rooms[x - 1, y] == MapFactory.Type.floor && rooms[x + 1, y] == MapFactory.Type.floor &&
+                             rooms[x, y - 1] == MapFactory.Type.floor )
+                            tileMap.SetTile( new Vector3Int( x, y, 0 ), north_walls[Random.Range( 0, north_walls.Length )] );
+
                         break;
                     case MapFactory.Type.floor:
-                        tileMapGround.SetTile( new Vector3Int( x, y, 0 ), floor );
+
+                        tileMap.SetTile( new Vector3Int( x, y, 0 ), floor[Random.Range( 0, floor.Length )] );
+
                         break;
                 }
             }
--- Roguelike-master/Roguelike/Packages/com.unity.2d.tilemap.extras/Tests/Editor/PlaceholderTests.cs
+++
@@ -1,7 +0,0 @@
-public class U2DExtrasPlaceholder 
-{
-    public void PlaceHolderTest()
-    {
-        //Assert.Pass("2D Extras tests are in a separate package.");
-    }
-}
