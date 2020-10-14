e3a07cb5c1cbd51c66f7a7425f26e151c46dbf76
Thimble <anthonyeboothroyd@gmail.com>
2020-10-14 04:34:41 +0100

    Update CameraTest.cs

--- Roguelike-master/Roguelike/Assets/Scripts/CameraTest.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/CameraTest.cs
@@ -1,48 +1,39 @@
-public float horizontalThreshold = 0.05f;
-public float verticalThreshold = 0.1f;
+using UnityEngine;
 
-void UpdateCamera()
+namespace TestOne
 {
-        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
-        
-        bool dirty = false;
-        
-        if(viewPos.x > 1.0f - horizontalTheshold)
+        public float horizontalThreshold = 0.05f;
+        public float verticalThreshold = 0.1f;
+
+        void UpdateCamera()
         {
-                viewPos.x = 1.0f - horizontalThreshold;
-                dirty = true;
-        }
-        
-        if(viewPos.x < horizontalThreshold)
-        {
-                viewPos.x = horizontalThreshold;
-                dirty = true;
-        }
-        
-        if(viewPos.y > 1.0f - vericalThreshold)
-        {
-                viewPos.y = 1.0f - verticalThreshold;
-                dirty = true;
-        }
-        
-        if(dirty)
-        {
+                Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
                 Vector3 offset = Transform.position - Camera.main.ViewportToWorldPoint(viewPos);
                 mainCamera.transform.position += offset;
         }
-}
 
-void OnEnable()
-{
-        StartCoroutine(PostFixedUpdate());
-}
-
-IEnumerator PostFixedUpdate()
-{
-        while(true)
+        void OnEnable()
         {
-                yield return new WaitForFixedUpdate();
+                StartCoroutine(PostFixedUpdate());
+        }
+
+        IEnumerator PostFixedUpdate()
+        {
+                while(true)
+                {
+                        yield return new WaitForFixedUpdate();
+
+                        UpdateCamera();
+                }
+        }
+}
+
+namespace TestTwo
+{
+        public static Vector3 SuperSmoothLerp(Vector3 followOld, Vector3 targetOld, Vector3 targetNew, float elapsedTime, float lerpAmount)
+        {
+                Vector3 f = followOld - targetOld + ( targetNew - targetOld ) / ( lerpAmount * elapsedTime );
                 
-                UpdateCamera();
+                return targetNew - (targetNew - targetOld) / (lerpAmount * elapsedTime) + f * Mathf.Exp( -lerpAmount * elapsedTime);
         }
 }
