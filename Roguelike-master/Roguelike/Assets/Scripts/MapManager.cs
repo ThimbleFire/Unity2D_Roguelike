8225882983fdfe2d4b0ca8974d0c226259cfe710
Thimble <anthonyeboothroyd@gmail.com>
2022-12-09 03:47:29 +0000

    Update MapManager.cs
    yeah, getting there, sorta.

--- Roguelike-master/Roguelike/Assets/Scripts/MapManager.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/MapManager.cs
@@ -38,45 +38,47 @@ public class MapManager : MonoBehaviour
     private Chunk GetCompatibleChunk( )
     {
         // If there is no parent, return a random chunk
-
         if(placedChunks.Count == 0)
             return chunksInMemory[Random.Range( 0, chunksInMemory.Count )];
 
         // Get a list rooms with entrances / exits
-
         List<Chunk> placedChunksWithEntrances = placedChunks.FindAll( x => x.Entrance.Count > 0 );
         
         // Select a random room from that list 
-        
         Chunk parent = placedChunksWithEntrances[Random.Range( 0, placedChunksWithEntrances.Count )];
 
         // Select an exit from that room
-
         int exitIndex = Random.Range( 0, parent.Entrance.Count );
-        AccessPoint accessPoint = root.Entrance[exitIndex];
+        AccessPoint.Dir parentOutputDir = parent.Entrance[exitIndex].direction;
+        
+        // Get the access points from parent 
+        AccessPoint[] parentAP = GetAPFacing(parent, parentOutputDir);
         
         // Flip the exit direction
-        
-        AccessPoint.Dir childInputDir = Flip(accessPoint.direction);
+        AccessPoint.Dir childInputDir = Flip(parentAP.direction);
         
         // Find a room compatible for parent
+        Chunk child = GetChildRoom(childInputDir);
         
-        Chunk child = Untitled(childInputDir);
+        // Get the accessPoints from child
+        AccessPoint[] childAP = GetAPFacing(child, childInputDir);
         
-        // Get the accessPoint from child
+        // Calculate brush position so that parent and child opposite APs are adjacent, or something, idfk
         
-        foreach(AccessPoint accessPoint in chunk.Entrance)
-        {
-            if(accessPoint.direction == childInputDir)
-            {
-                AccessPoint childInput = accessPoint;
-                break;
-            }
-        }
-        
-        //brushPosition = root.Origin + root.Entrance[exitIndex].position;
+        brushPosition = parent.Origin;
+        brushPosition += parentAP[0].position;
 
         return null;
+        
+        /*
+        # # w w w # #       X = origin
+        #           #       w = access point
+        w           w       # = wall
+        w           w
+        w           w        
+        #           #
+        X # w w w # #
+        */
     }
 
     private AccessPoint.Dir Flip(AccessPoint.Dir direction)
@@ -96,7 +98,7 @@ public class MapManager : MonoBehaviour
         return AccessPoint.Dir.DOWN;
     }
 
-    private Chunk Untitled(AccessPoint.Dir direction)
+    private Chunk GetChildRoom(AccessPoint.Dir direction)
     {
         foreach(Chunk chunk in chunksInMemory)
         {
@@ -108,6 +110,21 @@ public class MapManager : MonoBehaviour
                 }
             }
         }    
+    }
+    
+    private AccessPoint[] GetAPFacing(Chunk chunk, AccessPoint.Dir dir)
+    {
+        List<AccessPoint> accessPoints = new List<AccessPoint>();
+    
+        foreach(AccessPoint accessPoint in chunk.Entrance)
+        {
+            if(accessPoint.direction == dir)
+            {
+                accessPoints.Add(accessPoint);
+            }
+        }
+        
+        return accessPoints.ToArray();
     }
 
     private void BuildChunk(Chunk chunk)
