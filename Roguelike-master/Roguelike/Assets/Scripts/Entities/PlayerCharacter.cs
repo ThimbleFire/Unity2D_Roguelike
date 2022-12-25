74fdcc7f7820d9ab680ff2e0d4e3f5b547dda907
Thimble <anthonyeboothroyd@gmail.com>
2022-12-25 02:54:33 +0000

    Update PlayerCharacter.cs

--- Roguelike-master/Roguelike/Assets/Scripts/Entities/PlayerCharacter.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/Entities/PlayerCharacter.cs
@@ -8,26 +8,43 @@ public class PlayerCharacter : Entity
         Speed = 4;
     }
 
-    public override void Action( Vector3Int playerCharacterCoordinates )
+    public override void Action( )
     {
+    }
+    
+    protected override void Move()
+    {    
         int disX = Mathf.Abs( TileMapCursor.SelectedTileCoordinates.x - _coordinates.x );
         int disY = Mathf.Abs( TileMapCursor.SelectedTileCoordinates.y - _coordinates.y );
-        
         int distance = disX + disY;
         
-        // If we're in melee range and there's something there, perform a melee action
-        if ( distance == 1 && Entities.Search( TileMapCursor.SelectedTileCoordinates ).Count > 0 )
-        {
-            Debug.Log( Name + " attack!" );
-            Entities.Step();
-        }
-        else // Else consider it a move action
-        {
-            chain = Pathfind.GetPath( _coordinates, TileMapCursor.SelectedTileCoordinates, false );
-            TileMapCursor.Hide();
-            HUDControls.Hide();
-            base.Action( playerCharacterCoordinates );
-        }
+        // If we're at the location then we don't need to move
+        if ( distance <= 0 )
+            return;
+        
+        chain = Pathfind.GetPath( _coordinates, TileMapCursor.SelectedTileCoordinates, false );
+        TileMapCursor.Hide();
+        HUDControls.Hide();
+        base.Move();
+    }
+    
+    protected override void Attack()
+    {
+        // If there are no enemies, return
+        if(Entities.Search( TileMapCursor.SelectedTileCoordinates ).Count <= 0)
+            return;
+    
+        int disX = Mathf.Abs( TileMapCursor.SelectedTileCoordinates.x - _coordinates.x );
+        int disY = Mathf.Abs( TileMapCursor.SelectedTileCoordinates.y - _coordinates.y );        
+        int distance = disX + disY;
+        
+        //If we're not in melee range, return
+        if ( distance != 1 )
+            return;
+            
+        Debug.Log( Name + " attack!" );
+        Entities.Step();
+            
     }
 
     protected override void OnStep()
