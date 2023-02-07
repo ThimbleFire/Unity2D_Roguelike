3a16001fd2dea805f3e22f0ef339c94fa3600d3c
Thimble <anthonyeboothroyd@gmail.com>
2023-02-07 21:38:11 +0000

    Update NPCImp.cs

--- Roguelike-master/Roguelike/Assets/Scripts/Entities/Units/NPCImp.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/Entities/Units/NPCImp.cs
@@ -1,15 +1,18 @@
 public class NPCImp : Navigator {
     private void Start() {
         Name = "Imp";
-        RangeOfAggression = 6;
         Level = 1;
+        
         DmgBasePhyMin = 2;
         DmgBasePhyMax = 3;
         AttackRating = 8;
-        DefenseRating = 1;
+        
         DefenseBase = 5;
         ChanceToBlock = 3;
-        Life_Current = 8;
+        
+        Experience_Current = 33; 
+        Life_Current = Random.Range(7, 12); //inclusive min, inclusive max
+        RangeOfAggression = 6;
         SpeedBase = 1;
     }
 }
