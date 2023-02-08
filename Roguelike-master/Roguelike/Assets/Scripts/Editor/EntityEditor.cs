2050ba984162a3cfdd349ae0dae7d66a10fc9a59
Thimble <anthonyeboothroyd@gmail.com>
2023-02-08 06:21:59 +0000

    Update EntityEditor.cs

--- Roguelike-master/Roguelike/Assets/Scripts/Editor/EntityEditor.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/Editor/EntityEditor.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class EntityEditor : EditorBase
 {
+    private const string LBL_NAME = "Name";
+    private const string LBL_LEVEL = "Level";
+    private const string LBL_SPEED = "Speed";    
+    private const string LBL_LIFE_MAX = "Maximum Life";
+    private const string LBL_LIFE_CURRENT = "Current Life";
+    private const string LBL_MANA_MAX = "Maximum Mana";
+    private const string LBL_MANA_CURRENT = "Current Mana";    
+    private const string LBL_RANGE_OF_AGGRESSION = "Range of Aggression";
+    private const string LBL_EXPERIENCE = "Experience Reward" ;    
+    private const string LBL_ITEM_FIND = "Item Find";
+    private const string LBL_MAGIC_FIND = "Magic Find";    
+    private const string LBL_RES_FIRE = "Fire Resistance";
+    private const string LBL_RES_COLD = "Cold Resistance";
+    private const string LBL_RES_LIGHT = "Lightning Resistance" ;
+    private const string LBL_RES_POISON = "Poison Resistance" ;
+    private const string LBL_RES_ALL = "All Elemental Resistance";    
+    private const string LBL_ATTACK_RATING = "Attack Rating" ;
+    private const string LBL_CHANCE_TO_BLOCK = "Chance to Block" ;
+    private const string LBL_DEFENSE = "Defense" ;    
+    private const string LBL_DMG_PHYSICAL_MIN = "Minimum Physical Damage" ;
+    private const string LBL_DMG_FIRE_MIN = "Minimum Fire Damage" ;
+    private const string LBL_DMG_COLD_MIN ="Minimum Cold Damage" ;
+    private const string LBL_DMG_LIGHT_MIN = "Minimum Lightning Damage" ;
+    private const string LBL_DMG_POISON_MIN = "Minimum Poison Damage" ;
+    private const string LBL_DMG_ELEMENTAL_ALL_MIN ="Minimum Elemental Damage" ;
+    private const string LBL_DMG_PHYSICAL_MAX = "Maximum Physical Damage" ;
+    private const string LBL_DMG_FIRE_MAX =  "Maximum Fire Damage" ;
+    private const string LBL_DMG_COLD_MAX = "Maximum Cold Damage" ;
+    private const string LBL_DMG_LIGHT_MAX =  "Maximum Lightning Damage" ;
+    private const string LBL_DMG_POISON_MAX =  "Maximum Poison Damage" ;
+    private const string LBL_DMG_ELEMENTAL_ALL_MAX = "Maximum Elemental Damage" ;
+    
     Vector2 scrollView;
 
     EntityReplacement activeEntity;
     TextAsset obj;
     UnityEngine.AnimatorOverrideController animatorOverrideController;
     string animatorOverrideControllerPath;
-    string animatorOverrideControllerFileName;
 
     [MenuItem("Window/Editor/Entities")]
     private static void ShowWindow()
@@ -95,12 +126,10 @@ public class EntityEditor : EditorBase
 
     private void Save()
     {
-//        string filePath = AssetDatabase.GetAssetPath(SpriteUI).Substring("Assets/Resources/".Length);
-//        filePath = filePath.Substring(0, filePath.Length - 4);
-//        activeEntity.SpriteUIFilename = SpriteUI == null ? string.Empty : filePath;
-//
-//        filePath = AssetDatabase.GetAssetPath(animatorOverrideController).Substring("Assets/Resources/".Length);
-//        activeEntity.animationName = animatorOverrideController == null ? string.Empty : filePath;
-//        XMLUtility.Save<Item>(activeEntity, "Items/", activeEntity.Name);
+        string filePath = AssetDatabase.GetAssetPath(animatorOverrideController).Substring("Assets/Resources/".Length);
+        filePath = filePath.Substring(0, filePath.Length - 4);
+        activeEntity.animatorOverrideControllerFileName = animatorOverrideController == null ? string.Empty : filePath;
+        
+        XMLUtility.Save<EntityReplacement>(activeEntity, "Entities/", activeEntity.Name);
     }
 }
