a0de03d6c5319fca63e838035218561cb835a4cd
Thimble <anthonyeboothroyd@gmail.com>
2023-03-01 03:25:13 +0000

    Create Interfaces.cs
    Added obsolete for old method. Added can be attacked with receive damage and die.

--- Roguelike-master/Roguelike/Assets/Scripts/Entities/Interfaces.cs
+++ Roguelike-master/Roguelike/Assets/Scripts/Entities/Interfaces.cs
@@ -3,12 +3,12 @@ Namespace AlwaysEast
     public interface IAnimate
     {
         Animator _animator { get; set; }
+
+        [Obsolete("Redundant method. Use OnAnimationEnd instead.", true)]
         void OnAnimationEnd( string message );
-        // void DestroyAfterDeathAnimation();
-        // Redundant , use OnAnimationEnd.
     }
 
-    public interface IMove
+    public interface ICanMove
     {
         public List<Node> _chain { get; set; }
         public SpriteRenderer _spriteRenderer { get; set; }
@@ -16,12 +16,18 @@ Namespace AlwaysEast
         void UpdateAnimator( Vector3Int dir );
     }
 
-    public interface IAttack
+    public interface ICanAttack
     {
-        
+
     }
 
-    public interface IEquip
+    public interface ICanBeAttacked
+    {
+        void ReceiveDamage(int incomingDamage, float attackerCombatRating, float attackerLevel );
+        void Die();
+    }
+
+    public interface ICanEquip
     {
     
     }
